9a0b6d6b713a3a0a91d4950a5394332c56bd4820
Quentin Yann PIDOUX <quentin.pidoux@gmail.com>
2015-02-24 20:14:45 +0100

    Retrait du dossier pod du git pour ne pas poluer le depot

--- Pods/Headers/Public/COSTouchVisualizer/COSTouchVisualizerWindow.h
+++
@@ -1,1 +0,0 @@
-../../../COSTouchVisualizer/Classes/COSTouchVisualizerWindow.h
--- Pods/Target Support Files/Pods-2048 Widget-COSTouchVisualizer/Pods-2048 Widget-COSTouchVisualizer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_2048_Widget_COSTouchVisualizer : NSObject
-@end
-@implementation PodsDummy_Pods_2048_Widget_COSTouchVisualizer
-@end
